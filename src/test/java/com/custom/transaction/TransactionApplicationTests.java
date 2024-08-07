03df16a1a4da826fba7bb634a5d3f342d047e738
Vasilis Soumakis <billsoumakis@gmail.com>
2024-08-08 00:03:57 +0300

    Fixing tests to clear out the in memory db before each test

--- src/test/java/com/custom/transaction/TransactionApplicationTests.java
+++ src/test/java/com/custom/transaction/TransactionApplicationTests.java
@@ -8,8 +8,10 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class TransactionApplicationTests {
 
   @Autowired
